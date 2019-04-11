90c455b8cef7c54aa2001dccfd99de6d34cb0187
morix1500 <rade1500@gmail.com>
2019-04-12 00:07:51 +0900

    とりあえず

--- ABC121/D/main.cpp
+++ ABC121/D/main.cpp
@@ -1,82 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-vector<int> to2(int num)
-{
-    vector<int> res;
-    while (true)
-    {
-        if (num <= 1)
-        {
-            res.push_back(1);
-            break;
-        }
-
-        int tmp = num % 2;
-        res.push_back(tmp);
-        if (tmp == 1)
-        {
-            num = (num - 1) / 2;
-        }
-        else
-        {
-            num /= 2;
-        }
-    }
-    return res;
-}
+typedef unsigned long long ll;
 
 int main()
 {
-    int A, B;
+    ll A, B;
     cin >> A >> B;
 
-    vector<vector<int>> c;
-    vector<int> cSize;
-    int maxSize = 0;
-
-    for (int i = A; i <= B; i++)
+    ll c = 0;
+    for (ll i = A; i <= B; i++)
     {
-        vector<int> tmp = to2(i);
-        c.push_back(tmp);
-        cSize.push_back(tmp.size());
-        if (maxSize < tmp.size())
+        if (i == A)
         {
-            maxSize = tmp.size();
+            c = A;
+            continue;
         }
+
+        c = c ^ i;
     }
 
-    int l = c.size();
-    vector<int> res(maxSize);
-
-    int index = 0;
-    while (true)
-    {
-        int x = 0;
-        int err = 0;
-        for (int i = 0; i < l; i++)
-        {
-            if (index >= cSize.at(i))
-            {
-                err++;
-                continue;
-            }
-            x += c.at(i).at(index);
-        }
-        res[index] = x % 2;
-        if (err >= maxSize)
-        {
-            break;
-        }
-        index++;
-    }
-    int num = 0;
-    for (int i = 0; i < res.size(); i++)
-    {
-        if (res.at(i) == 1)
-        {
-            num += pow(2, i);
-        }
-    }
-    cout << num << endl;
+    cout << c << endl;
 }
