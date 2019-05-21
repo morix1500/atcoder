d3bc4bbe02408eda91bf57e95834f77e7a1f1590
morix1500 <rade1500@gmail.com>
2019-05-21 21:23:44 +0900

    abc083c

---
+++ atcoder/ABC083/C/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ll x, y;
+  cin >> x >> y;
+
+  ll cnt = 0;
+  for (ll i = x; i <= y; i *= 2) {
+    cnt++;
+  }
+  cout << cnt << endl;
+}
