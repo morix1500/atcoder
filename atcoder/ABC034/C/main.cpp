33b774e9a017f3c1b82eeac37f85244c0e200cba
morix1500 <rade1500@gmail.com>
2019-06-17 21:38:24 +0900

    abc034

--- atcoder/ABC034/C/main.cpp
+++ atcoder/ABC034/C/main.cpp
@@ -3,9 +3,14 @@ using namespace std;
 typedef long long ll;
 static const ll MOD = 1000000007;
 
-ll factorial(ll n) {
-  if (n == 0) return 1;
-  return n % MOD * factorial(n - 1) % MOD;
+int pow(ll i, ll j) {
+  ll tmp = 1;
+  while (j) {
+    if (j % 2) tmp = tmp * i % MOD;
+    i = i * i % MOD;
+    j /= 2;
+  }
+  return tmp;
 }
 
 int main() {
@@ -13,5 +18,10 @@ int main() {
   cin >> W >> H;
   W--;
   H--;
-
+  ll a = 1, b = 1;
+  for (int i = 0; i < W; i++)
+    a = a * (W + H - i) % MOD;
+  for (int i = 2; i <= W; i++)
+    b = b * i % MOD;
+  cout << a * pow(b, MOD - 2) % MOD << endl;
 }
